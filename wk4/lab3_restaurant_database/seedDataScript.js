284f64109d263f65a7f66057449e6d72dc274c5f
SeltzerJon <bestfriends90@gmail.com>
2024-02-04 21:25:10 -0500

    Expose DB

--- wk4/lab3_restaurant_database/seedDataScript.js
+++ wk4/lab3_restaurant_database/seedDataScript.js
@@ -9,7 +9,7 @@ const Restaurant = require('./models/restaurant.js');
 const testDataPath = path.join(__dirname, 'testData.json');
 const testData = JSON.parse(fs.readFileSync(testDataPath, 'utf-8'));
 
-const DB_CONNECTION_STRING = 'mongodb+srv://bestfriends90:fziIQqlaqmrcIwmv@cluster0.2yrqiyw.mongodb.net/f2023_comp3133?retryWrites=true&w=majority';
+const DB_CONNECTION_STRING = '';
 mongoose.connect(DB_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
