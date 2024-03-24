1b5d378ac665d455eb7874552b1d2ee144b2273c
Jon <bestfriends90@gmail.com>
2024-03-24 19:35:07 -0400

    wk11

---
+++ wk11/angular-tour-of-heroes/src/app/input-format.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, HostListener, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[inputFormat]'
+})
+export class InputFormatDirective {
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('blur') onBlur() {
+    const inputValue: string = this.el.nativeElement.value;
+    this.el.nativeElement.value = inputValue.toUpperCase();
+  }
+}
